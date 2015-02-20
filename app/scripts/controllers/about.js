d63577f4b798fd4efa7327215e4b73bf8d487b53
benson <vhailior@gmail.com>
2015-02-20 21:58:18 +0000

    Initial scaffold

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name diplomacyApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the diplomacyApp
+ */
+angular.module('diplomacyApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name diplomacyApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the diplomacyApp
+ */
+angular.module('diplomacyApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
